b9ad224acb455667b354fc828b5b8cca668bd2f0
DESKTOP-T935DM3\PC <toantran03720@gmail.com>
2025-02-11 16:51:07 +0700

    h

--- Front-end/src/components/NotFound/NotFound.jsx
+++ Front-end/src/components/NotFound/NotFound.jsx
@@ -1,9 +1,10 @@
 import Lottie from "lottie-react";
 import notfound from "../../assets/animation/NotFound404.json";
+import "./NotFound.scss"
 const NotFound = () => {
   return (
     <>
-      <Lottie style={{height: "100vh"}} animationData={notfound} loop={true} />;
+      <Lottie style={{height: "100vh"} } animationData={notfound} loop={true} className="not_found"/>;
     </>
   );
 };
