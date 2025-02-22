d20cbd5c434a8450a3bc234073abb1b33993002d
Xululu <xululu2002@gmail.com>
2025-02-22 17:15:40 +0700

    dd

--- Front-end/constants.js
+++ Front-end/constants.js
@@ -6,7 +6,7 @@ if (process.env.BUILD_MODE === "dev") {
   apiRoot = "http://localhost:3000";
 }
 if (process.env.BUILD_MODE === "production") {
-  apiRoot = "https://km-kinhmat.onrender.com";
+  apiRoot = "https://km-kinhmat-1.onrender.com";
 }
 console.log("api_root", apiRoot);
 export const API_ROOT = apiRoot;
